f5173c86d0ba0488cb6be3f318d2a809b379cb9f
FabGPT <fabgpt.inbox@gmail.com>
2026-04-21 12:01:45 +0200

    feat(testing): add jest configuration [chore] [gitoma]

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: [
+    'jest-environment-node',
+  ],
+  // Add other configurations as needed for TypeScript/Rust related testing if applicable
+};

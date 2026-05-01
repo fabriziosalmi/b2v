aa803c18bcc27ee480e33660fd05c0c771a365e9
FabGPT <fabgpt.inbox@gmail.com>
2026-05-01 08:13:10 +0200

    chore(eslint): integrate Prettier with ESLint [gitoma]

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'prefer-const': 'warn',
+    },
+  },
+  prettier
+);
